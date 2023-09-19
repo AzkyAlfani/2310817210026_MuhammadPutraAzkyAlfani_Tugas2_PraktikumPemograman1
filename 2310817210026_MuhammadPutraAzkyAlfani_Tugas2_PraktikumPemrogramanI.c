28b27b59a878a981e5748e6d0e15f190fbf22738
Al <144989156+AzkyAlfani@users.noreply.github.com>
2023-09-19 20:03:35 +0800

    Add files via upload

---
+++ 2310817210026_MuhammadPutraAzkyAlfani_Tugas2_PraktikumPemrogramanI.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Selamat Pagi,\nTeknologi Informasi FTULM Selalu dihati\n");
+    return 0;
+}
+
+
+
---
+++ 2310817210026_MuhammadPutraAzkyAlfani_Tugas2_PraktikumPemrogramanI.py
@@ -0,0 +1,2 @@
+print("selamat pagi,")
+print("Teknologi Informasi FTULM Selalu dihati")
